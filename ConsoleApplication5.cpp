011bda695ff7839cec35097202606d69dec3f87d
Elisey-e <58297256+Elisey-e@users.noreply.github.com>
2022-08-25 20:04:58 +0300

    Add files via upload

--- ConsoleApplication5.cpp
+++ ConsoleApplication5.cpp
@@ -1,31 +1,50 @@
-﻿#include <stdio.h>
+﻿/*!
+\file
+\brief Главный файл проекта с функцией main
+\authors Zhdanov_EA
+
+Данный файл содержит в себе функцию main, решающую квадратное уравнение
+*/
+
+
+#include <stdio.h>
 #include <locale.h>
-#include <math.h>
-#include <assert.h>
 #include "squaresolver.h"
 
 
+/*!
+	\brief Главная функция
+*/
 int main()
 {
 	setlocale(LC_ALL, "Rus");
+
 	double indices[3] = { 0, 0, 0 };	/* Массив коэффициентов уравнения */
 	double x1 = 0, x2 = 0;				/* Корни */
+
 	printf("Введите коэффициенты квадратного уравнения вида ax^2 + bx + c = 0 по порядку\n");
 	while (scanf("%lf %lf %lf", &indices[0], &indices[1], &indices[2]) != 3) {
 		printf("Ввод некорректен, произведите ввод заново\n");
 		while (getchar() != '\n') {}
 	}
+
 	printf("Решение уравнения ");
 	int count = Solve_Square(indices[0], indices[1], indices[2], &x1, &x2);
+
 	Reduce_To_0(&x1);
 	Reduce_To_0(&x2);
+
 	switch (count) {
-	case 2: printf("\nx1 = %g\nx2 = % g\n", x1, x2);
+	case 2:
+		printf("\nx1 = %g\nx2 = % g\n", x1, x2);
 		break;
-	case 1: printf("\nx = %g\n", x1);
+	case 1:
+		printf("\nx = %g\n", x1);
 		break;
-	case 0: printf("\nПустое множество\n.");
+	case 0:
+		printf("\nПустое множество\n.");
 		break;
-	case INF_ROOTS: printf("\nБесконечное множество\n.");
+	case INF_ROOTS: 
+		printf("\nБесконечное множество\n.");
 	}
 }
--- squaresolver.h
+++ squaresolver.h
@@ -1,125 +1,58 @@
+/*!
+\file
+\brief Заголовочный файл с описанием функций
+
+Данный файл содержит в себе определения основных 
+функций, используемых при решении квадратного уравнения
+*/
+
+
+
 #define INF_ROOTS -1
 #define ASSERT(cond) \
 	if (!(cond))\
 		printf("\tError in line %d\n\tof the file %s\n\tcondinion (%s) is false\n", __LINE__, __FILE__, #cond);
 
 
-int  Eps_Comp(double a, double b);											/* Сравнение чисел с точностью до эпсилон*/
-void Print_Square_Problem(double a, double b, double c);					/* Вывод квадратного уравнения*/
-int  Solve_Square(double a, double b, double c, double* x1, double* x2);	/* Расчет решений квадратного уравнения*/
-void Print_Linear_Problem(double b, double c);								/* Вывод линейного уравнения*/
-void Reduce_To_0(double* x);												/* Функция, приводящая ответ -0 к 0*/
+/*!
+	\brief Функция, которая проверяет присутствие 2-х чисел в одной эпсилон-окрестности
+	\param[in] a, b Два числа, которые мы сравниваем
+	\return 1 если оба числа принадлежат одной окрестности или 0 если нет
+*/
+int  Eps_Comp(double a, double b);
+
+
+/*!
+	\brief Функция, которая распечатывает квадратное уравнение с подставленными числовыми коэффициентами
+	\param[in] a, b, c Три числа - коэффициента уравнения
+*/
+void Print_Square_Problem(double a, double b, double c);
+
+
+/*!
+	\brief Функция, которая решает квадратное уравнение, линейное в случае равенства коэффициента при x^2 нулю
+	\param[in] a, b, c Три числа - коэффициента уравнения и две ссылки на его корни
+	\return Количество решений уравнения, INF_ROOTS если корней бесконечно много
+*/
+int  Solve_Square(double a, double b, double c, double* x1, double* x2);
+
+
+/*!
+	\brief Функция, которая распечатывает линейное уравнение с подставленными числовыми коэффициентами
+	\param[in] b, c Два числа - коэффициента уравнения
+*/
+void Print_Linear_Problem(double b, double c);
+
+
+/*!
+	\brief Функция, изменяющая число на 0 в случае его равенства -0
+	\param[in] x Ссылка на изменяемое число
+*/
+void Reduce_To_0(double* x);
+
+
+/*!
+	\brief Функция, тестирующая функцию Solve_Square
+	Обычный дочерний класс, который отнаследован от ранее созданного класса Parent
+*/
 void TestSquare();
-
-
-void TestSquare()
-{
-	double x1 = 0, x2 = 0;
-	int nRoots = Solve_Square(1, 0, -4, &x1, &x2);
-	ASSERT(nRoots == 2 && Eps_Comp(x1, -2) && Eps_Comp(x2, 2));
-}
-
-
-int Eps_Comp(double a, double b) {
-	if (fabs(a - b) <= 0.00001) {
-		return 1;
-	}
-	return 0;
-}
-
-
-void Print_Square_Problem(double a, double b, double c) {
-	if (Eps_Comp(a, 1)) {
-		printf("x^2");
-	}
-	else if (Eps_Comp(a, -1)) {
-		printf("-x^2");
-	}
-	else printf("%gx^2", a);
-	if (Eps_Comp(b, 1)) {
-		printf(" + x");
-	}
-	else if (Eps_Comp(b, -1)) {
-		printf(" - x");
-	}
-	else if (b < 0) {
-		printf(" - %gx", -b);
-	}
-	else if (!Eps_Comp(b, 0)) {
-		printf(" + %gx", b);
-	}
-	if (Eps_Comp(c, -1)) {
-		printf(" - 1");
-	}
-	else if (c < 0) {
-		printf(" - %g", -c);
-	}
-	else if (!Eps_Comp(c, 0)) {
-		printf(" + %g", c);
-	}
-	printf(" = 0");
-}
-
-
-int Solve_Square(double a, double b, double c, double* x1, double* x2) {
-	double D = b * b - 4 * a * c;
-	double sqd = sqrt(D);
-	if (Eps_Comp(a, 0)) {
-		Print_Linear_Problem(b, c);
-		if (Eps_Comp(b, 0)){
-			if (Eps_Comp(c, 0)){
-				return INF_ROOTS;
-			}
-			else{
-				return 0;
-			}
-		}
-		else {
-			*x1 = -c / b;
-			return 1;
-		}
-	}
-	Print_Square_Problem(a, b, c);
-	if (Eps_Comp(D, 0)) {
-		*x1 = -b / (2 * a);
-		return 1;
-	}
-	else if (D > 0) {
-		*x1 = (-b - sqd) / (2 * a);
-		*x2 = (-b + sqd) / (2 * a);
-		return 2;
-	}
-	else if (D < 0) {
-		return 0;
-	}
-}
-
-
-void Print_Linear_Problem(double b, double c) {
-	if (Eps_Comp(b, -1)) {
-		printf("-x");
-	}
-	else if (Eps_Comp(b, 1)) {
-		printf("x");
-	}
-	else {
-		printf("%gx", b);
-	}
-	if (Eps_Comp(c, -1)) {
-		printf(" - 1");
-	}
-	else if (c < 0) {
-		printf(" - %g", -c);
-	}
-	else if (!Eps_Comp(c, 0)) {
-		printf(" + %g", c);
-	}
-	printf(" = 0");
-}
-
-
-void Reduce_To_0(double* x) {
-	if (Eps_Comp(*x, 0)) {
-		*x = 0;
-	}
-}
